2b7ef5a89bde0f451cf5ff97389ea2a3f3f41ec3
ivansteezy <network.9961@gmail.com>
2023-11-21 11:55:27 -0600

    working example

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@ int main()
   MagickWandGenesis();
 
   MagickWand* wand = NewMagickWand();
-  if(MagickReadImage(wand, "./../images/1.jpg") == MagickFalse)
+  if(MagickReadImage(wand, "./../images/abstrakt.png") == MagickFalse)
   {
     ExceptionType severity;
     char *description = MagickGetException(wand, &severity);
@@ -22,20 +22,41 @@ int main()
     exit(1);
   }
 
-  if (MagickTransformImageColorspace(wand, GRAYColorspace) == MagickFalse) {
-      ExceptionType severity;
-      char *description = MagickGetException(wand, &severity);
+  MagickWand* imageCanvas = NewMagickWand();
+  PixelWand* pw = NewPixelWand();
+  PixelSetColor(pw, "white");
+  if(MagickNewImage(imageCanvas, 1170 / 2, 2532 / 2, pw) == MagickFalse)
+  {
+    ExceptionType severity;
+    char *description = MagickGetException(wand, &severity);
 
-      // Imprimimos el mensaje de error
-      fprintf(stderr, "Error: %s (Severity: %d)\n", description, severity);
+    // Imprimimos el mensaje de error
+    fprintf(stderr, "Error: %s (Severity: %d)\n", description, severity);
 
-      // Liberar la memoria utilizada para el mensaje de error
-      description = MagickRelinquishMemory(description);
-      fprintf(stderr, "Error al transformar la imagen a escala de grises\n");
-      exit(1);
+    // Liberar la memoria utilizada para el mensaje de error
+    description = MagickRelinquishMemory(description);
+    fprintf(stderr, "Error al crear el canvas\n");
+    exit(1);
+  }
+  
+  MagickWand* res = NewMagickWand();
+  res = MagickTextureImage(imageCanvas, wand);
+  if (!res) 
+  {
+    ExceptionType severity;
+    char *description = MagickGetException(wand, &severity);
+
+    // Imprimimos el mensaje de error
+    fprintf(stderr, "Error: %s (Severity: %d)\n", description, severity);
+
+    // Liberar la memoria utilizada para el mensaje de error
+    description = MagickRelinquishMemory(description);
+    fprintf(stderr, "Error al transformar la imagen a tiles\n");
+    exit(1);
   }
 
-  if (MagickWriteImage(wand, "./../images/output_gray.jpg") == MagickFalse) {
+  if (MagickWriteImage(res, "./../images/output_gray2.png") == MagickFalse) 
+  {
       ExceptionType severity;
       char *description = MagickGetException(wand, &severity);
 
@@ -49,9 +70,10 @@ int main()
       exit(1);
   }
 
-  wand = DestroyMagickWand(wand);
+  DestroyPixelWand(pw);
+  DestroyMagickWand(wand);
+  DestroyMagickWand(imageCanvas);
 
   MagickWandTerminus();
-  printf("Hola mundo!"); 
   return 0;
 }
